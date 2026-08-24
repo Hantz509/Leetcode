22403de415f1ca3ddcf4d8cda8ac177bb5b8adf9
Hantz509 <guerrierhantz58@gmail.com>
2026-08-24 20:47:15 +0800

    This is Leetcode 9's problem. It is about verifying if a number is a palindrome.

---
+++ Leetcode_9.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x < 0){
+            return false;
+        }
+        int save = x;
+        long long y = 0;
+        while(x > 0 ){
+            y = y * 10 + x % 10;
+            x = x / 10;
+        }
+       return y == save;
+    }
+};
